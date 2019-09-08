2f45a3e733c47d9301400b172a22d896eb8e675c
Nick <nicholasgregory@live.co.uk>
2019-09-08 21:54:37 +0100

    Challenge editor - added table of challenge info

--- src/containers/pages/Admin/ChallengeEditor.js
+++ src/containers/pages/Admin/ChallengeEditor.js
@@ -5,6 +5,7 @@ import Page from "../bases/Page";
 import theme from "theme";
 
 import Button from "../../../components/Button.js"
+import Input from "../../../components/Input.js"
 
 const Results = styled.div`
     background-color: ${theme.bg_d2};
@@ -12,6 +13,10 @@ const Results = styled.div`
     height: 500px;
     margin: 25px auto;
     padding: 5px;
+
+    * {
+        margin: 2px auto;
+    }
 `
 
 export default () => <Page title={'Edit Challenges'}>
@@ -21,10 +26,35 @@ export default () => <Page title={'Edit Challenges'}>
         <option value='c1'>Challenge 1</option>
         <option value='c1'>Challenge 1</option>
     </select>
-    <Button>Go</Button>
 
     <Results>
-        <h2>Challenge 1</h2>
+        <table>
+            <tr>
+                <td>Name: </td>
+                <td><Input placeholder={"Challenge 1"}></Input></td>
+            </tr>
+            <tr>
+                <td>Points: </td>
+                <td><Input placeholder={"6969"}></Input></td>
+            </tr>
+            <tr>
+                <td>Description: </td>
+                <td><Input placeholder={"This is a great challenge description"} rows={'5'}></Input></td>
+            </tr>
+            <tr>
+                <td>Flag Hash: </td>
+                <td><Input placeholder={"abc132"}></Input></td>
+            </tr>
+            <tr>
+                <td>Hint: </td>
+                <td><Input placeholder={"kali linux illegal"}></Input></td>
+            </tr>
+            <tr>
+                <td>Flag Format: </td>
+                <td><Input placeholder={"thebeanogamer"}></Input></td>
+            </tr>
+        </table>
+        <Button>Save</Button>
     </Results>
 
 
